5e9fc76567571f90c8a9bcd8c6f06df9fa035504
fakynno <fakynno@gmail.com>
2025-04-16 23:36:09 -0300

    Importe inicial do projeto

---
+++ src/main/java/com/toystore/product/ToystoreProductApplication.java
@@ -0,0 +1,13 @@
+package com.toystore.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToystoreProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToystoreProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toystore/product/ToystoreProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toystore.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToystoreProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
