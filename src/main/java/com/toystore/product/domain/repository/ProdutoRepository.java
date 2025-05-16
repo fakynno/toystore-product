97893dd2b30dc37bf9157008fb6079b63e82a15e
fakynno <fakynno@gmail.com>
2025-05-15 23:53:04 -0300

    adicionada busca personalizada

--- src/main/java/com/toystore/product/domain/repository/ProdutoRepository.java
+++ src/main/java/com/toystore/product/domain/repository/ProdutoRepository.java
@@ -4,6 +4,11 @@ import com.toystore.product.domain.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Object> findBySku(String sku);
+
+    boolean existsByNome(String nome);
 }
