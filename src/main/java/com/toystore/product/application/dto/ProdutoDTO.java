237d14ec974888aade2eb8199c0019df88aa59bd
fakynno <fakynno@gmail.com>
2025-05-15 23:39:28 -0300

    ajustes no DTO para validacoes

--- src/main/java/com/toystore/product/application/dto/ProdutoDTO.java
+++ src/main/java/com/toystore/product/application/dto/ProdutoDTO.java
@@ -2,8 +2,13 @@ package com.toystore.product.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
 
 public record ProdutoDTO(
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
@@ -11,41 +16,41 @@ public record ProdutoDTO(
     Long produtoId,
 
     @NotBlank(message = "O nome do produto é obrigatório.")
-    @Min(value = 3, message = "O nome do produto deve ter no mínimo 3 caracteres.")
+    @Size(min = 3, message = "O nome do produto deve ter no mínimo 3 caracteres.")
     @Schema(example = "Produto 1")
     String nome,
 
     @NotBlank(message = "A marca do produto é obrigatória.")
-    @Min(value = 3, message = "A marca do produto deve ter no mínimo 3 caracteres.")
+    @Size(min = 3, message = "A marca do produto deve ter no mínimo 3 caracteres.")
     @Schema(example = "Marca 1")
     String marca,
 
     @NotBlank(message = "A cor do produto é obrigatória.")
-    @Min(value = 4, message = "A cor do produto deve ter no mínimo 4 caracteres.")
+    @Size(min = 4, message = "A cor do produto deve ter no mínimo 4 caracteres.")
     @Schema(example = "Azul")
     String cor,
 
     @NotBlank(message = "O tamanho do produto é obrigatório.")
-    @Min(value = 1, message = "O tamanho do produto deve ter no mínimo 1 caractere.")
+    @Size(min = 1, message = "O tamanho do produto deve ter no mínimo 1 caractere.")
     @Schema(example = "M")
     String tamanho,
 
     @NotBlank(message = "A faixa etária do produto é obrigatória.")
-    @Min(value = 1, message = "A faixa etária do produto deve ter no mínimo 1 caractere.")
+    @Size(min = 1, message = "A faixa etária do produto deve ter no mínimo 1 caractere.")
     @Schema(example = "3 anos")
     String faixaEtaria,
 
     @Min(value = 2, message = "A quantidade de peças do produto deve ser maior do que 2.")
     @Schema(example = "10")
-    int quantidadePecas,
+    Integer quantidadePecas,
 
-    @NotBlank(message = "O preço do produto é obrigatório.")
-    @Min(value = 1, message = "O preço do produto deve ser maior do que 1.")
-    @Schema(example = "99.99")
-    String preco,
+    @NotNull(message = "O preço do produto é obrigatório.")
+    @DecimalMin(value = "1.00", message = "O preço do produto deve ser maior ou igual a R$1.00.")
+    @Schema(example = "1.00")
+    BigDecimal preco,
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    @Schema(example = "MAR-PRO-AZU-M-3")
+    @Schema(example = "MAR-PRO-AZUL-M-3-3DE6")
     String skuDoProduto
 ) {
 }
